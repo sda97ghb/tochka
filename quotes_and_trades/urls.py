167a43eadd892f8ae1cabdd8f890e5995da7b714
sda97g <sda97g@gmail.com>
2019-03-18 00:46:12 +0500

    Added quotes, insider, insiders, analytics site pages.

--- quotes_and_trades/urls.py
+++ quotes_and_trades/urls.py
@@ -6,9 +6,8 @@ app_name = 'quotes_and_trades'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:ticker_id>/', views.quotes, name='quotes'),
-    path('<int:ticker_id>/insider', views.insiders, name='insiders'),
-    path('<int:ticker_id>/insider/<str:insider>', views.insider, name='insider'),
+    path('<int:ticker_id>/insider/', views.insiders, name='insiders'),
+    path('<int:ticker_id>/insider/<str:insider>/', views.insider, name='insider'),
+    path('<int:ticker_id>/analytics', views.analytics, name='analytics'),
+    path('<int:ticker_id>/delta', views.delta, name='delta'),
 ]
-
-# /%TICKER%/analytics?date_from=..&date_to=... будет отдавать веб-страницу с данными о разнице цен в текущих датах(нужна разница всех цен - открытия, закрытия, максимума, минимума)
-# /%TICKER/delta?value=N&type=(open/high/low/close) будет отдавать веб-страницу с данными о минимальных периодах (дата начала-дата конца), когда указанная цена изменилась более чем на N
--- quotes_and_trades/views.py
+++ quotes_and_trades/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from datetime import date, timedelta, datetime
+
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 from .models import Ticker
@@ -6,7 +8,8 @@ from .models import Ticker
 
 def index(request):
     """
-    / будет отдавать веб-страницу со ссылками на акции, доступные в базе данных.
+    /
+    будет отдавать веб-страницу со ссылками на акции, доступные в базе данных.
     """
     return render(request, 'quotes_and_trades/index.html', context={
         'tickers': Ticker.objects.all()
@@ -15,23 +18,118 @@ def index(request):
 
 def quotes(request, ticker_id):
     """
-    /%TICKER% будет отдавать веб-страницу с таблицей цен на акцию за 3 месяца
+    /%TICKER%
+    будет отдавать веб-страницу с таблицей цен на акцию за 3 месяца
     """
     ticker = Ticker.objects.get(id=ticker_id)
-    return HttpResponse("WIP: %s" % ticker.name)
+    today = date.today()
+    three_month_before = today + timedelta(days=-3*365/12)
+    last_three_month_quotes = ticker.quote_set.filter(date__range=[three_month_before, today])
+    return render(request, 'quotes_and_trades/quotes.html', context={
+        'ticker': ticker,
+        'quotes': last_three_month_quotes
+    })
 
 
 def insiders(request, ticker_id):
     """
-    /%TICKER%/insider будет отдавать веб-страницу с данными торговли владельцев компании. На эту страницу попадать по ссыле со страницы /%TICKER%/
+    /%TICKER%/insider
+    будет отдавать веб-страницу с данными торговли владельцев компании.
+    На эту страницу попадать по ссыле со страницы /%TICKER%/
     """
     ticker = Ticker.objects.get(id=ticker_id)
-    return HttpResponse("WIP: %s" % ticker.name)
+    trades = ticker.trade_set.all()
+    return render(request, 'quotes_and_trades/insiders.html', context={
+        'ticker': ticker,
+        'trades': trades
+    })
 
 
 def insider(request, ticker_id, insider):
     """
-    /%TICKER%/insider/%NAME% будет отдавать веб-страницу с данными о торговле данного владельца компании. На эту страницу попадать по ссылке со страницы /%TICKER%/insider
+    /%TICKER%/insider/%NAME%
+    будет отдавать веб-страницу с данными о торговле данного владельца компании.
+    На эту страницу попадать по ссылке со страницы /%TICKER%/insider
     """
     ticker = Ticker.objects.get(id=ticker_id)
-    return HttpResponse("WIP: %s" % ticker.name)
+    trades = ticker.trade_set.filter(insider=insider)
+    return render(request, 'quotes_and_trades/insider.html', context={
+        'ticker': ticker,
+        'insider': insider,
+        'trades': trades
+    })
+
+
+def analytics(request: HttpRequest, ticker_id):
+    """
+    /%TICKER%/analytics?date_from=..&date_to=...
+    будет отдавать веб-страницу с данными о разнице цен в текущих датах
+    (нужна разница всех цен - открытия, закрытия, максимума, минимума)
+    """
+    date_from = get_date_or_today(request, 'date_from')
+    date_to = get_date_or_today(request, 'date_to')
+    ticker = Ticker.objects.get(id=ticker_id)
+    quotes = ticker.quote_set.filter(date__range=[date_from, date_to])
+    if quotes.count() > 0:
+        first_quote = quotes[0]
+        last_quote = first_quote
+        open = first_quote.open
+        close = open
+        low = open
+        high = open
+        for quote in quotes:
+            last_quote = quote
+            close = quote.close
+            low = min(low, quote.low)
+            high = max(high, quote.high)
+        return render(request, 'quotes_and_trades/analytics.html', context={
+            'ticker': ticker,
+            'date_from': date_from,
+            'date_to': date_to,
+            'today': date.today(),
+            'date_from_iso': date_to_string(date_from),
+            'date_to_iso': date_to_string(date_to),
+            'today_iso': date_to_string(date.today()),
+            'no_data_flag': False,
+            'first_quote': first_quote,
+            'last_quote': last_quote,
+            'statistics': {
+                'open': open,
+                'close': close,
+                'low': low,
+                'high': high,
+                'change': close - open
+            }
+        })
+    else:
+        return render(request, 'quotes_and_trades/analytics.html', context={
+            'ticker': ticker,
+            'date_from': date_from,
+            'date_to': date_to,
+            'today': date.today(),
+            'date_from_iso': date_to_string(date_from),
+            'date_to_iso': date_to_string(date_to),
+            'today_iso': date_to_string(date.today()),
+            'no_data_flag': True,
+        })
+
+
+def date_to_string(date):
+    return "%d-%02d-%02d" % (date.year, date.month, date.day)
+
+
+def get_date_or_today(request, argument_name):
+    today = date_to_string(date.today())
+    try:
+        return datetime.strptime(request.GET.get(argument_name, today), '%Y-%m-%d')
+    except ValueError:
+        return datetime.strptime(today, '%Y-%m-%d')
+
+
+def delta(request, ticker_id):
+    """
+    /%TICKER/delta?value=N&type=(open/high/low/close)
+    будет отдавать веб-страницу с данными о минимальных периодах (дата начала-дата конца),
+    когда указанная цена изменилась более чем на N
+    """
+    return None
